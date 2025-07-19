e4a81c0920dda01c9337988ccadb320a98fcd280
zuekveer <simetramonk@gmail.com>
2025-07-19 21:54:06 +0300

    task4

--- task4/dryKiss.go
+++ task4/dryKiss.go
@@ -1,5 +1,7 @@
 /*
 Что нарушает принцип DRY: скопированный код или дублирующая бизнес-логика в разных функциях?
+Оба
+
 
 Упростите этот код, избегая дублирования (DRY + KISS):
 func CalcPriceA(qty int) int {
@@ -9,3 +11,15 @@ func CalcPriceB(qty int) int {
 	return qty*100 + qty*10 + 5
 }
 */
+
+func calcPrice(qty, basePrice, percent, extra int) int {
+    return qty*basePrice + qty*percent + extra
+}
+
+func CalcPriceA(qty int) int {
+    return calcPrice(qty, 100, 10, 0)
+}
+
+func CalcPriceB(qty int) int {
+    return calcPrice(qty, 100, 10, 5)
+}
