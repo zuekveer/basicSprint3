9020e0e595eaace22781d82cd4a4d3a1422bf092
zuekveer <simetramonk@gmail.com>
2025-07-19 22:04:04 +0300

    task4

--- task4/solid.go
+++ task4/solid.go
@@ -10,3 +10,25 @@ func (r Report) Print() {}       // ← вынести
 func (r Report) ExportJSON() {}  // ← вынести
 
 */
+
+type Report struct {
+	Title string
+	Body  string
+}
+
+type Printer struct{}
+
+func (p Printer) Print(report Report) {
+    fmt.Printf("Отчёт: %s\n%s\n", report.Title, report.Body)
+}
+
+type JSONExporter struct{}
+
+func (e JSONExporter) ExportJSON(report Report) []byte {
+    data := map[string]string{
+        "title": report.Title,
+        "body":  report.Body,
+    }
+    jsonData, _ := json.Marshal(data)
+    return jsonData
+}
