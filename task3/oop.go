d62b57abc22ac0838ad9b447af509011ee65647b
zuekveer <simetramonk@gmail.com>
2025-07-19 21:49:26 +0300

    task3

--- task3/oop.go
+++ task3/oop.go
@@ -1,6 +1,6 @@
 /*
 Как в Go реализуется наследование, если самого наследования нет?
-
+1. через embedding
 
 Создай структуру `Animal` с методом `Speak()`, и структуру `Dog`, которая "наследует" поведение `Animal`, но переопределяет `Speak()`:
 
@@ -12,3 +12,30 @@ type Dog struct {
 }
 func (d Dog) Speak() string { return "Woof" }
 */
+
+package task3
+
+import "fmt"
+
+type Animal struct {
+}
+
+func (a Animal) Speak() string {
+	return "..."
+}
+
+type Dog struct {
+	Animal
+}
+
+func (d Dog) Speak() string {
+	return "Woof!"
+}
+
+func main() {
+	animal := Animal{}
+	dog := Dog{}
+
+	fmt.Println(animal.Speak())
+	fmt.Println(dog.Speak())
+}
